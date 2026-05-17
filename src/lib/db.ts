9b8dbab273554c4c2282bcb559a3232972c531e3
Michael Martz <mmartz@sonatype.com>
2026-05-17 16:48:07 -0500

    fix: remove invalid dexie event listeners causing crash

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -8,19 +8,10 @@ export class PTODatabase extends Dexie {
 
   constructor() {
     super('PTODatabase');
-    console.log('DB: Initializing PTODatabase...');
     this.version(1).stores({
       entries: '++id, startDate, endDate',
       resets: '++id, asOfDate',
       settings: '++id'
-    });
-
-    this.on('ready', () => {
-      console.log('DB: Database is ready');
-    });
-
-    this.on('error', (err) => {
-      console.error('DB: Database error:', err);
     });
   }
 }
